146dacdf6af4913fb48c5d46245800c553d222fa
Andy <andyhelloandy@gmail.com>
2024-08-28 01:42:03 -0400

    Rename wordlist files and read them on startup

--- src/game/wordList/wordList.ts
+++ src/game/wordList/wordList.ts
@@ -1,9 +1,10 @@
 import { createReadStream } from "fs"
 import { createInterface } from "readline"
 import { once } from "events"
+import { assert } from "../../util/assert";
 
-async function readWordList() {
-    const rs = createReadStream("src/game/wordList/wordList.txt") as unknown as NodeJS.ReadableStream; // Evil type cast
+async function readWordList(path: string) {
+    const rs = createReadStream(path) as unknown as NodeJS.ReadableStream; // Evil type cast
 
     const wordList: string[] = [];
 
@@ -14,9 +15,11 @@ async function readWordList() {
 
 
     rl.on("line", line => {
+        assert(typeof (line) === "string");
         if (line.startsWith("#") || line.length === 0) {
             return;
         }
+        line = line.toLowerCase().replace(/[^a-z]/g, ""); // Not the fastest but this only needs to be run once at the beginning of the process.
         wordList.push(line);
     })
 
@@ -27,10 +30,19 @@ async function readWordList() {
 
 /** The word pool for the game. */
 export const wordList: Set<string> = new Set();
+export const commonWordList: Set<string> = new Set();
 
-/** Populates {@link wordList} with words. Must be called exactly once. */
+/**
+ * Populates {@link wordList} and {@link commonWordList} with words. Must be called
+ * exactly once.
+ */
 export async function initWordList(): Promise<void> {
-    (await readWordList())
+    (await readWordList("src/game/wordList/wordsWordnik.txt"))
         .forEach(word => wordList.add(word));
+    (await readWordList("src/game/wordList/words3of6game.txt"))
+        .forEach(word => {
+            wordList.add(word);
+            commonWordList.add(word);
+        });
 }
 
