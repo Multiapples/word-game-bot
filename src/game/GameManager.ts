a418c0c9000565e53b73e791c229182a88f97dd5
Multiapples <andyhelloandy@gmail.com>
2024-09-02 19:38:33 -0400

    Check player limit

--- src/game/GameManager.ts
+++ src/game/GameManager.ts
@@ -19,19 +19,26 @@ export class GameManager {
      * Otherwise, starts the game immediately.
      */
     async tryNewGame(guild: Guild, players: User[], interaction: CommandInteraction): Promise<void> {
-        const guildGames: Collection<UserId, Game> = this.games.ensure(guild.id, () => new Collection());
-        // Ensure players are not in a game already.
-        if (!players.every(user => !guildGames.has(user.id))) {
-            await autoReply(interaction, {
-                content: "Could not create game: A user is already in a game."
-            })
-            return;
-        }
         // Ensure interaction is coming from a guild text channel.
         if (interaction.channel === null || interaction.channel.isDMBased() || !interaction.channel.isTextBased()) {
             await autoReply(interaction, {
                 content: "Could not create game: This is not a server text channel.",
             });
+            return;
+        }
+        // Ensure players limit is not exceeded.
+        if (players.length > Game.maxPlayers) {
+            await autoReply(interaction, {
+                content: `Could not create game: Too many players. The maximum is ${Game.maxPlayers}.`,
+            })
+            return;
+        }
+        // Ensure players are not in a game already.
+        const guildGames: Collection<UserId, Game> = this.games.ensure(guild.id, () => new Collection());
+        if (!players.every(user => !guildGames.has(user.id))) {
+            await autoReply(interaction, {
+                content: "Could not create game: A user is already in a game."
+            })
             return;
         }
         // Create new game.
