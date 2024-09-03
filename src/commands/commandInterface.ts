0fe4035215944e07b63d3cb9ca4e8a2b9c9f7ff6
Multiapples <andyhelloandy@gmail.com>
2024-09-02 20:07:31 -0400

    Change slashCommand property type

--- src/commands/commandInterface.ts
+++ src/commands/commandInterface.ts
@@ -1,6 +1,6 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder } from "discord.js";
 
 export interface Command {
-    readonly slashCommand: SlashCommandBuilder;
+    readonly slashCommand: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
     execute(interaction: CommandInteraction): Promise<void>;
 }
