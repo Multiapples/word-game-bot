55def862a68de6cb4725f36a0323f4c26d357b29
Multiapples <andyhelloandy@gmail.com>
2024-09-02 20:31:45 -0400

    Setup multiple players

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,12 +1,23 @@
 import { SlashCommandBuilder, User, Guild, ChatInputCommandInteraction } from "discord.js";
 import { Command } from "./commandInterface";
 import { getGameManager } from "../game/GameManager";
+import { Game } from "../game/Game";
 
 export class PlayCommand implements Command {
 
-    slashCommand = new SlashCommandBuilder()
-        .setName("play")
-        .setDescription("Starts a game");
+    slashCommand = (() => {
+        const slashCommand = new SlashCommandBuilder()
+            .setName("play")
+            .setDescription("Starts a game");
+        // Having a bunch of userOptions to invite other players is very unelegant
+        // but it's the easiest way to get it working.
+        for (let player = 2; player <= Game.maxPlayers; player++) {
+            slashCommand.addUserOption(option => option
+                .setName(`player${player}`)
+                .setDescription("Play with another user"));
+        }
+        return slashCommand;
+    })();
 
     async execute(interaction: ChatInputCommandInteraction): Promise<void> {
         if (interaction.guild === null) {
@@ -18,6 +29,20 @@ export class PlayCommand implements Command {
         }
         const guild: Guild = interaction.guild;
         const players: User[] = [interaction.user];
+        for (let player = 2; player <= Game.maxPlayers; player++) {
+            const user: User | null = interaction.options.getUser(`player${player}`, false);
+            if (user === null) {
+                continue;
+            }
+            if (players.includes(user)) {
+                await interaction.reply({
+                    content: "You included yourself or the same player twice.",
+                    ephemeral: true,
+                });
+                return;
+            }
+            players.push(user);
+        }
         await getGameManager().tryNewGame(guild, players, interaction);
     }
 }
